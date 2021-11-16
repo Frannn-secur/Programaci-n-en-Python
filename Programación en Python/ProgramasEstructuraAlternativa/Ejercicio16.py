dd442f4e6c0e9af654052ecbf86ccdb33f76939c
FranciscoJoséGómezRuiz <a20gorufr@iesgrancapitan.org>
2021-11-16 17:17:01 +0100

    Ejercicios 15 y 16 añadidos

---
+++ Programación en Python/ProgramasEstructuraAlternativa/Ejercicio16.py
@@ -0,0 +1,16 @@
+"""
+
+Creado el día 08/11/2021
+
+La política de cobro de una compañía telefónica es: cuando se realiza una llamada, el cobro es por el tiempo que ésta
+dura, de tal forma que los primeros cinco minutos cuestan 1 euro por minuto, los siguientes tres, 80 céntimos por minuto
+, los siguientes dos minutos, 70 céntimos por minuto, y a partir del décimo minuto, 50 céntimos por minuto.
+
+Además, se carga un impuesto de 3% cuando es domingo, y si es otro día, en turno de mañana, 15%, y en turno de tarde,
+10%. Realice un algoritmo para determinar cuánto debe pagar por cada concepto una persona que realiza una llamada.
+
+Algoritmo:
+
+1º Pedimos los minutos, dia y horario
+2º Calculamos si dura más de 10min
+"""
