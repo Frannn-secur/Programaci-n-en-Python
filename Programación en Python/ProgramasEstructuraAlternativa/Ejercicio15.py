63f4461a7676fe36303868a209693ace279549fe
FranciscoJoséGómezRuiz <a20gorufr@iesgrancapitan.org>
2021-11-16 17:59:47 +0100

    Arreglados y bien formados los ejercicios 15 y 16

--- Programación en Python/ProgramasEstructuraAlternativa/Ejercicio15.py
+++ Programación en Python/ProgramasEstructuraAlternativa/Ejercicio15.py
@@ -2,9 +2,39 @@
 
 Creado el día 08/11/2021
 
+Hecho por Francisco José Gómez Ruiz
+
 El director de una escuela está organizando un viaje de estudios, y requiere determinar cuánto debe cobrar a cada alumno
 y cuánto debe pagar a la compañía de viajes por el servicio. La forma de cobrar es la siguiente: si son 100 alumnos o
 más, el costo por cada alumno es de 65 euros; de 50 a 99 alumnos, el costo es de 70 euros, de 30 a 49, de 95 euros, y
 si son menos de 30, el costo de la renta del autobús es de 4000 euros, sin importar el número de alumnos. Realiza un
 programa que permita determinar el pago a la compañía de autobuses y lo que debe pagar cada alumno por el viaje.
+
+Algoritmo:
+1º Lo primero que haríamos sería pedir el numero de Alumnos, y ahora hacemos lo siguiente
+2º Si el número de alumnos que nos han dado fuese mayor o igual a 100 sacamos por pantalla --> El pago a la agencia es
+de (multiplicamos el numero de alumnos por 65 porque es lo que paga cada alumno)
+3º Si no, el numero de alumnos es de 99 a 50 nos sacará por pantalla --> El pago a la agencia es de (multiplicamos el
+numero de alumnos por 70 euros porque sería lo que pagaría cada uno)
+4º Si no, el número de los alumnos es de 49 a 30  sacamos por pantalla --> El pago a la agencia es de (multiplicamos el
+numero de alumnos por 95 porque es lo que pagaría cada alumno)
+5º Si no, el número sacamos por pantalla --> Como el numero de alumnos es menor de 30 costará el pago de la agencia
+4000/numeroalumnos
+
 """
+
+print("Vamos a sacar el valor que nos va a costar la agencia de autobuses: ")
+
+numero_alumnos = int(input("Dame el número de alumnos que irían de viaje: "))
+
+if numero_alumnos >= 100:
+    print("El pago a la agencia es de ", numero_alumnos * 65, "euros y cada alumno de los que irian al viaje pagaría "
+                                                              "65 por persona")
+elif 100 > numero_alumnos >= 50:
+    print("El pago a la agencia es de ", numero_alumnos * 70, "euros y cada alumno de los que irían al viaje pagaría "
+                                                              "70 por persona")
+elif 50 > numero_alumnos >= 30:
+    print("El pago a la agencia es de ", numero_alumnos * 95, "euros y cada alumno de los que irían al viaje pagaría "
+                                                              "95 por persona")
+else:
+    print("El coste de la agencia del autobús es de 4000 euros  y cada alumno pagaría: ", 4000/numero_alumnos, "€")
--- Programación en Python/ProgramasEstructuraAlternativa/Ejercicio16.py
+++ Programación en Python/ProgramasEstructuraAlternativa/Ejercicio16.py
@@ -1,6 +1,8 @@
 """
 
-Creado el día 08/11/2021
+Creado el día 08/11/202
+
+Hecho por Francisco José Gómez Ruiz
 
 La política de cobro de una compañía telefónica es: cuando se realiza una llamada, el cobro es por el tiempo que ésta
 dura, de tal forma que los primeros cinco minutos cuestan 1 euro por minuto, los siguientes tres, 80 céntimos por minuto
@@ -11,6 +13,45 @@ Además, se carga un impuesto de 3% cuando es domingo, y si es otro día, en tur
 
 Algoritmo:
 
-1º Pedimos los minutos, dia y horario
-2º Calculamos si dura más de 10min
+1º Preguntamos el tiempo que ha estado en llamada
+2º Le preguntamos al usuario si es domingo, si NO es domingo y tampoco es por la tarde el cargo del impuesto es de 0.15,
+si no es domingo y es por la tarde el impuesto es de 0.1
+3º Si la llamada se ha realizado el domingo el cargo por minuto será 0.3
+4º Si el tiempo ha sido menos de 5 minutos, el precio de la llamada será el tiempo durado * 100
+5º Si no, el tiempo ha sido igual o menor de 8 minutos la llamada, el precio será el tiempo durado - 5 (porque lo que va
+a costar mas sería los siguiente 3 min) * 80 y le sumamos a ese resultado 500 (que sería 5 min = 5€
+6º Si no, el tiempo ha sido igual o menor de 10 minutos la llamada, el precio será el tuempo durado - 8 (por que los
+siguientes 2 min seran de 70 cent) y le sumariamos 740 del resto de los minutos
+7º Si no, el tiempo sería 10 min y por lo cual el ultimo min costaria 50 cent por lo que sería 880 + (tiempo-10) * 50
+
 """
+
+print("Programa que calcula el precio de una llamada.")
+
+tiempo = int(input("Inserta los minutos que ha durado dicha llamada: "))
+
+# Comprobamos el impuesto a añadir.
+domingo = input("¿Es domingo?(S/N) ")
+if domingo.upper() == "N":
+    tarde = input("¿Es turno de tardes?(S/N) ")
+    if tarde.upper() == "N":
+        # Impuestos de mañanas.
+        impuesto = 0.15
+    else:
+        # Impuestos de tardes.
+        impuesto = 0.1
+else:
+    # Impuestos de domingos.
+    impuesto = 0.03
+
+# Calculamos el coste de la llamada.
+if tiempo <= 5:
+    precio = tiempo * 100
+elif tiempo <= 8:
+    precio = 500 + (tiempo-5) * 80
+elif tiempo <= 10:
+    precio = 740 + (tiempo-8) * 70
+else:
+    precio = 880 + (tiempo-10) * 50
+
+print(f"El precio final de la llamada es {precio * (1 + impuesto) / 100:.2f} €")
