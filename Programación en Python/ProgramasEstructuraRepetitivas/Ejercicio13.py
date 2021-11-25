a2c702a990ed69c179e57481e3322856f7ad12ec
FranciscoJoséGómezRuiz <a20gorufr@iesgrancapitan.org>
2021-11-25 11:12:54 +0100

    Ejercicio 13, convierte dentro de la cadena los caracteres en minusculas en mayusculas y viceversa

--- Programación en Python/ProgramasEstructuraRepetitivas/Ejercicio13.py
+++ Programación en Python/ProgramasEstructuraRepetitivas/Ejercicio13.py
@@ -6,9 +6,22 @@ Hecho por Francisco José Gómez Ruiz
 
 Realizar un programa que lea una cadena por teclado y convierta las mayúsculas a minúsculas y viceversa.
 
-1º Pedimos la cadena
-2º Recorremos la cadena con el fin de contar los espacios que tiene y cuando detecte uno, aumentamos nuestra variable
-contador de espacios
-3º Después mostraremos dicha variable para decir cuantas palabras hay en la cadena insertada.
+1º Pedimos la cadena.
+2º Recorremos la cadena para saber si estamos enfrente de un caracter en minúscula o en mayúscula.
+3º Después usaremos la función swapcase para convertir dicho caracter en su contrario.
+4º Lo sacamos por pantalla
 
 """
+
+print("Pedimos una cadena para transformarla segun este, pasando los caracteres minúsculos a mayúsculas y viceversa: ")
+
+cadena = str(input("Dame la cadena: "))
+cadena_final = ""
+
+# Usamos los métodos islower y isupper para ver si la cadena fuese minuscula o mayuscula
+for i in cadena:
+    if i.islower() or i.isupper():
+        # Con la funcion swapcase nos quitamos de problemas ya que lo que sea i lo convertirá a lo otro y viceversa.
+        cadena_final += i.swapcase()
+
+print(f"Esta sería la cadena nueva: {cadena_final}")
